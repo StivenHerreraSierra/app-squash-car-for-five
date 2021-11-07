81fe993f6ff3ca7488ff0a9ffd12b5f70fb72c7d
Vi-ocoder <vicotov@gmail.com>
2021-11-07 17:40:01 -0500

    Se cambio el formato aplicando el usado por Stiven

--- frontend/js/registro.js
+++ frontend/js/registro.js
@@ -1,20 +1,30 @@
-function guardar() {
-    var mensaje = confirm("Los datos se guardaran...");
-    if (mensaje) {
-        alert("Esta es una prueba, en realidad no se guardó nada");
-    } else {
-        alert("¡Cancelado!");
+function limpiar() {
+    var inputs = ["inputNombre", "inputApellido", "inputNumeroDocumento", "inputTelefono"];
+    for (let i = 0; i < inputs.length; i++) {
+        document.getElementById(inputs[i]).value = "";
+        document.getElementById("span" + i).innerHTML = "x";
     }
+    vistaPrevia();
 }
 
 function vistaPrevia() {
 
-    var inputs = ["inputNombre", "inputApellido", "selectTipoDocumento", "inputNumeroDocumento", "inputTelefono", "selectTipoVehiculo", "inputPlacaVehiculo"];
-    var ids = ["nombreEscrito", "apellidoEscrito", "tipoDocEscrito", "numDocEscrito", "telefonoEscrito", "tipoVehEscrito", "placaEscrito"];
+    var inputs = ["inputNombre", "inputApellido", "selectTipoDocumento", "inputNumeroDocumento", "inputTelefono"];
+    var ids = ["nombreEscrito", "apellidoEscrito", "tipoDocEscrito", "numDocEscrito", "telefonoEscrito"];
     for (let i = 0; i < inputs.length; i++) {
         var idX = document.getElementById(inputs[i]).value;
         if (idX != "") {
             document.getElementById(ids[i]).innerHTML = idX;
+            if (i == 2) {
+                document.getElementById("span" + i).innerHTML = "x";
+            } else {
+                if (i > 2) {
+                    document.getElementById("span" + (i - 1)).innerHTML = "Ok";
+                } else {
+                    document.getElementById("span" + i).innerHTML = "Ok";
+                }
+            }
+
         } else {
             document.getElementById(ids[i]).innerHTML = "Sin Escribir";
         }
