8ce2e878df0137545167fa7dde0e377236c06595
marioalopezo <malo1986@msn.com>
2021-11-21 19:57:16 -0500

    ajustes conexion MongoDB

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,24 +3,29 @@ require("dotenv").config();
 
 const mongoose = require("mongoose");
 
-/*
-const host = "localhost";
-const port = "27017";
-const db = "hr";
-*/
-
 exports.mongoConnect = () => {
-    
-    
-    //const mongoStringConnection = `mongodb://${host}:${port}/${db}`;
-    const mongoStringConnection = process.env.DB_URI;
+  //
 
-    mongoose.connect(mongoStringConnection)
-    .then(() => console.log("Conexión exitosa"))
-    .catch(err => console.error(err));
-    
-    mongoose.Promise = global.Promise;
-    const dbConnection = mongoose.connection;
-    dbConnection.on("error", console.error.bind
-    (console, "Mongodb connection error"))
-}
+  let mongoStringConnection = "";
+
+  if (process.env.DB_URI == undefined) {
+    const host = "localhost";
+    const port = "27017";
+    const db = "hr";
+    mongoStringConnection = `mongodb://${host}:${port}/${db}`;
+  } else {
+    mongoStringConnection = process.env.DB_URI;
+  }
+
+  mongoose
+    .connect(mongoStringConnection)
+    .then(() => console.log("Conexión exitosa a MongoDB"))
+    .catch((err) => console.error(err));
+
+  mongoose.Promise = global.Promise;
+  const dbConnection = mongoose.connection;
+  dbConnection.on(
+    "error",
+    console.error.bind(console, "Mongodb connection error")
+  );
+};
