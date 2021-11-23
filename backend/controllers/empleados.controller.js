979bc5212ab746f3b6d239825c59132e533791d3
marioalopezo <malo1986@msn.com>
2021-11-22 20:30:38 -0500

    ajustes conexion mongo db

--- backend/controllers/empleados.controller.js
+++ backend/controllers/empleados.controller.js
@@ -1,5 +1,8 @@
 const Empleado = require("../models/empleados.model");
 
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 let response = {
   msg: "",
   exito: false,
@@ -9,9 +12,11 @@ exports.create = function (req, res) {
   let empleado = new Empleado({
     nombres: req.body.nombres,
     apellidos: req.body.apellidos,
-    tipo_documento: req.body.tipo_documento,
-    numero_documento: req.body.numero_documento,
+    tipoDocumento: req.body.tipoDocumento,
+    numeroDocumento: req.body.numeroDocumento,
     telefono: req.body.telefono,
+    pass: req.body.pass
+
   });
 
   empleado.save(function (err) {
@@ -22,11 +27,60 @@ exports.create = function (req, res) {
       res.json(response);
       return;
     }
-    
+
     response.exito = true;
     response.msg = "El empleado se guardó correctamente";
     res.json(response);
-
-
   });
+};
+
+exports.find = function (req, res) {
+  Empleado.find(function (err, empleados) {
+    res.json(empleados);
+  });
+};
+
+exports.findOne = function (req, res) {
+  Empleado.findOne({ _id: req.params.id }, function (err, empleado) {
+    res.json(empleado);
+  });
+};
+
+exports.remove = function (req, res) {
+  Empleado.findByIdAndDelete({ _id: req.params.id }, function (err) {
+    if (err) {
+      console.log(err);
+      response.exito = false;
+      response.msg = "Error al eliminar el empleado";
+      res.json(response);
+      return;
+    }
+
+    response.exito = true;
+    response.msg = "El empleado ha sido eliminado";
+    res.json(response);
+  });
+};
+
+
+exports.login = function (req, res) {
+  const numeroDocumentoEmpleado = req.body.numeroDocumento;
+  const empleado = Empleado.findOne({ numeroDocumento: numeroDocumentoEmpleado });  
+
+  const match = bcrypt.compare(numeroDocumentoEmpleado, empleado.numeroDocumento);
+
+  if (match) {
+    const token = jwt.sign({ _id: empleado._id }, "Secreta");
+    res.json({
+      mensaje: "Bienvenido",
+      id: empleado._id,
+      nombres: empleado.nombres,
+      apellidos: empleado.apellidos, 
+      token: token,
+    });
+  } else {
+    res.json({
+      mensaje: "Credenciales inválidas",
+    });
+  }
 };
--- backend/models/clientes.model.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const ClientesSchema = new Schema({});
-
-module.exports = mongoose.model("clientes", ClientesSchema);
