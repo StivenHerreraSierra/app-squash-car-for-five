f2bccfb9510a997fdf8fe169cf8d82be761783c0
marioalopezo <malo1986@msn.com>
2021-11-23 23:38:52 -0500

    ajuste bootstrap en App.js evitar sobreescritura

--- Frontend-react/src/App.js
+++ Frontend-react/src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from 'react-bootstrap';
 /*import Navegacion from './components/navegacion/Navegacion';
 import Header from './components/header/header';
 import HistorialCliente from './components/cliente/historial/HistorialCliente';*/
 import AppRouter from './components/router/router';
-import "bootstrap/dist/css/bootstrap.min.css";
+
 
 
 /*
