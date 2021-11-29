8d3e680b7fcc8767e671fdf4af09d7f813fb183d
StivenHerreraSierra <stiven.herreras@uqvirtual.edu.co>
2021-11-28 21:29:56 -0500

    Agregar servicio

--- Frontend-react/src/components/cliente/reservas/FormularioCliente.js
+++ Frontend-react/src/components/cliente/reservas/FormularioCliente.js
@@ -7,45 +7,23 @@ export default class FormularioCliente extends React.Component {
       <div className="formulario">
         <Form>
           <Row className="mb-3">
-            <Form.Group as={Col} controlId="formGridCedula">
-              <Form.Label for="inputCedula">Cédula</Form.Label>
+            <Form.Group as={Col}>
+              <Form.Label>Cédula *</Form.Label>
               <Form.Control
                 type="number"
                 id="inputCedula"
                 disabled
-                value="89182341"
+                value={this.props.cliente.idCliente}
               />
             </Form.Group>
 
-            <Form.Group as={Col} controlId="formGridNombre">
-              <Form.Label for="inputNombre">Nombre</Form.Label>
+            <Form.Group as={Col}>
+              <Form.Label>Nombre *</Form.Label>
               <Form.Control
                 type="text"
                 id="inputNombre"
                 disabled
-                value="Nombre del cliente"
-              />
-            </Form.Group>
-          </Row>
-
-          <Row className="mb-3 justify-content-start">
-            <Form.Group as={Col} controlId="formGridTelPpal">
-              <Form.Label className="label" for="inputTelefono1">
-                Teléfono principal
-              </Form.Label>
-              <Form.Control
-                type="tel"
-                className="form-control"
-                id="inputTelefono1"
-              />
-            </Form.Group>
-
-            <Form.Group as={Col} controlId="formGridTelSec">
-              <Form.Label for="inputTelefono2">Teléfono secundario</Form.Label>
-              <Form.Control
-                type="tel"
-                className="form-control"
-                id="inputTelefono2"
+                value={this.props.cliente.nombreCliente}
               />
             </Form.Group>
           </Row>
--- Frontend-react/src/components/cliente/reservas/FormularioServicio.js
+++ Frontend-react/src/components/cliente/reservas/FormularioServicio.js
@@ -2,45 +2,75 @@ import React from "react";
 import { Form, Row, Col } from "react-bootstrap";
 
 export default class Formulario extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.onFechaChange = this.onFechaChange.bind(this);
+    this.onPlacaChange = this.onPlacaChange.bind(this);
+    this.onTipoVehiculoChange = this.onTipoVehiculoChange.bind(this);
+    this.onTipoLavadoChange = this.onTipoLavadoChange.bind(this);
+    this.onObservacionChange = this.onObservacionChange.bind(this);
+  }
+
+  onFechaChange(e) {
+    this.props.onFechaChange(e.target.value);
+  }
+
+  onPlacaChange(e) {
+    this.props.onPlacaChange(e.target.value.trim());
+  }
+
+  onTipoVehiculoChange(e) {
+    this.props.onTipoVehiculoChange(e.target.value);
+  }
+
+  onTipoLavadoChange(e) {
+    this.props.onTipoLavadoChange(e.target.value);
+  }
+
+  onObservacionChange(e) {
+    this.props.onObservacionChange(e.target.value.trim());
+  }
+
   render() {
     return (
       <div className="formulario">
         <Form>
           <Row>
-            <Form.Group as={Col} className="mb-3" controlId="formGridFecha">
-              <Form.Label>Fecha</Form.Label>
-              <Form.Control type="date" />
+            <Form.Group as={Col} className="mb-3">
+              <Form.Label>Fecha *</Form.Label>
+              <Form.Control type="date" value={this.props.servicio.fecha} onChange={this.onFechaChange}/>
             </Form.Group>
           </Row>
 
           <Row>
-            <Form.Group as={Col} controlId="formGridPlaca">
-              <Form.Label for="inputPlaca" className="form-label">
-                Placa
+            <Form.Group as={Col}>
+              <Form.Label className="form-label">
+                Placa *
               </Form.Label>
-              <Form.Control type="text" id="inputPlaca"></Form.Control>
+              <Form.Control type="text" id="inputPlaca" value={this.props.servicio.placa} onChange={this.onPlacaChange}></Form.Control>
             </Form.Group>
 
-            <Form.Group as={Col} className="mb-3" controlId="formGridTipoVehiculo">
-              <Form.Label for="inputTipo">Tipo de vehículo</Form.Label>
-              <select className="form-select" id="inputTipo">
+            <Form.Group as={Col} className="mb-3">
+              <Form.Label>Tipo de vehículo *</Form.Label>
+              <Form.Select className="form-select" id="inputTipo" value={this.props.servicio.tipoVehiculo} onChange={this.onTipoVehiculoChange}>
                 <option defaultValue value="Automóvil">
                   Automóvil
                 </option>
                 <option value="Bicicleta">Bicicleta</option>
-                <option value="Bicicleta">Motocarro</option>
+                <option value="Motocarro">Motocarro</option>
                 <option value="Motocicleta">Motocicleta</option>
                 <option value="Otros">Otros</option>
-              </select>
+              </Form.Select>
             </Form.Group>
           </Row>
 
           <Row>
-            <Form.Group as={Col} className="mb-3" controlId="formGridFecha">
-              <label for="inputTipo" className="form-label">
-                Tipo de lavado
+            <Form.Group as={Col} className="mb-3">
+              <label className="form-label">
+                Tipo de lavado *
               </label>
-              <select className="form-select" id="inputTipo">
+              <select className="form-select" id="inputTipo" value={this.props.servicio.tipoLavado} onChange={this.onTipoLavadoChange}>
                 <option defaultValue value="Convencional">Convencional</option>
                 <option value="Completo">Completo</option>
                 <option value="Ecologico">Ecológico</option>
@@ -49,14 +79,16 @@ export default class Formulario extends React.Component {
           </Row>
 
           <Row>
-            <Form.Group as={Col} className="mb-3" controlId="formGridFecha">
-              <Form.Label for="inputObservaciones">
+            <Form.Group as={Col} className="mb-3">
+              <Form.Label>
                 Observaciones/Detalles
               </Form.Label>
               <textarea
                 className="form-control"
                 id="inputObservaciones"
                 rows="3"
+                onChange={this.onObservacionChange}
+                value={this.props.servicio.observaciones}
               ></textarea>
             </Form.Group>
           </Row>
